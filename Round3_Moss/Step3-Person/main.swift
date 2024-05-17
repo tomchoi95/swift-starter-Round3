6f194d9dabad73994c1df2a7269040d010f49557
peat <ldc1000@naver.com>
2024-05-17 19:16:48 +0900

    코드 수정

--- Round3_Moss/Step3-Person/main.swift
+++ Round3_Moss/Step3-Person/main.swift
@@ -6,20 +6,20 @@
 //
 import Foundation
 
-var misterLee = Person(name: "misterLee" ,birthYears: 1990, job: "barista", money: 20_000)
-var missKim = Person(name: "missKim" ,birthYears: 1999, job: "student", money: 50_000)
+let misterLee = Person(name: "misterLee" ,birthYears: 1990, job: "barista", money: 20_000)
+let missKim = Person(name: "missKim" ,birthYears: 1999, job: "student", money: 50_000)
 
-var yagombucks = CoffeeShop(sales: 0,
-                            menuBoard: [.americano(temp: .ice): 4500,
-                                        .americano(temp: .hot): 4800,
-                                        .latte(temp: .hot): 4800,
+let yagombucks = CoffeeShop(sales: 0,
+                            menuBoard: [.americano(temperature: .ice): 4500,
+                                        .americano(temperature: .hot): 4800,
+                                        .latte(temperature: .hot): 4800,
                                         .coldbrew: 5000],
                             pickUpTable: [:],
                             barista: misterLee)
 
-print("missKim 의 잔액: ", missKim.money,"yagombucks 의 매출액: ", yagombucks.sales)
-missKim.orderCoffee(at: yagombucks, [.americano(temp: .ice): 5])
-print("missKim 의 잔액: ", missKim.money,"yagombucks 의 매출액: ", yagombucks.sales)
+//print("missKim 의 잔액: ", missKim.money,"yagombucks 의 매출액: ", yagombucks.sales, "픽업대: ", yagombucks.pickUpTable)
+missKim.orderCoffee(at: yagombucks, [.americano(temperature: .ice): 5])
+//print("missKim 의 잔액: ", missKim.money,"yagombucks 의 매출액: ", yagombucks.sales, "픽업대: ", yagombucks.pickUpTable)
 
 
 class Person {
@@ -36,26 +36,27 @@ class Person {
     }
     
     func orderCoffee(at coffeeShop: CoffeeShop ,_ orders: [Coffee: Int]) {
-        var coffeeShop = coffeeShop
+        guard let totalPrice = coffeeShop.takeOrder(orders, customer: self) else { return }
         
-        let totalPrice = coffeeShop.takeOrder(orders, customer: self)
+        guard enoughMoney(charge: totalPrice) else { return }
         
-        guard totalPrice != -1 else {return}
-        
-        guard pay(charge: totalPrice) else {return}
+        pay(charge: totalPrice)
         
         coffeeShop.payed(charge: totalPrice)
         
         coffeeShop.makeCoffee(orders: orders, customer: self)
     }
     
-    func pay(charge: Int) -> Bool {
+    func enoughMoney(charge: Int) -> Bool {
         guard money > charge else {
             print("잔액이 \(charge-money)원만큼 부족합니다.")
             return false
         }
-        self.money -= charge
         return true
+    }
+    
+    func pay(charge: Int) {
+        self.money -= charge
     }
 }
 
@@ -72,27 +73,31 @@ class CoffeeShop {
         self.barista = barista
     }
     
-    func takeOrder(_ orders: [Coffee: Int], customer: Person) -> Int {
+    func takeOrder(_ orders: [Coffee: Int], customer: Person) -> Int? {
         guard Set(menuBoard.keys).isSuperset(of: Set(orders.keys)) else {
             let haveNoMenus = Set(orders.keys)
-                .filter {!Set(menuBoard.keys).contains($0)}
-                .map {$0.convertKor()}
+                .filter { !Set(menuBoard.keys).contains($0) }
+                .map { $0.convertKorean() }
             
             print("죄송합니다. \(haveNoMenus.joined(separator: ", "))는 준비되지 않은 메뉴입니다.")
-            return -1
+            return nil
         }
         
-        let totalPrice: Int = orders.map {menuBoard[$0.key]! * $0.value}.reduce(0, +)
+        let totalPrice: Int = orders.map{
+            (menuBoard[$0.key] ?? 0) * $0.value
+        } .reduce(0, +)
+        
         print("총 \(totalPrice)원 결제 도와드리겠습니다.")
         return totalPrice
     }
     
     func makeCoffee(orders: [Coffee:Int], customer: Person) {
-        let orderStr = orders.map {$0.key.convertKor() + " " + String($0.value) + "잔"}
-        self.pickUpTable = orders.reduce(into: pickUpTable) {
-            result, order in
-            result[order.key] = (result[order.key] ?? 0) + order.value}
-            
+        let orderStr = orders.map { $0.key.convertKorean() + " " + String($0.value) + "잔" }
+        
+        self.pickUpTable = orders.reduce(into: pickUpTable) { result, order in
+            result[order.key] = (result[order.key] ?? 0) + order.value
+        }
+ 
         print(customer.name +
               " 님이 주문하신 " +
               orderStr.joined(separator: ", ") +
@@ -106,27 +111,27 @@ class CoffeeShop {
 }
 
 enum Coffee: Hashable {
-    case americano(temp: CoffeeTemperature)
-    case latte(temp: CoffeeTemperature)
+    case americano(temperature: CoffeeTemperature)
+    case latte(temperature: CoffeeTemperature)
+    case vanillaLatte(temperature: CoffeeTemperature)
     case coldbrew
-    case vanillaLatte(temp: CoffeeTemperature)
     
-    func convertKor() -> String {
+    func convertKorean() -> String {
         switch self {
-        case .americano(temp: .hot):
+        case .americano(temperature: .hot):
             return "따뜻한 아메리카노"
-        case .americano(temp: .ice):
+        case .americano(temperature: .ice):
             return "아이스 아메리카노"
-        case .latte(temp: .hot):
+        case .latte(temperature: .hot):
             return "따뜻한 라떼"
-        case .latte(temp: .ice):
+        case .latte(temperature: .ice):
             return "아이스 라떼"
+        case .vanillaLatte(temperature: .hot):
+            return "따뜻한 바닐라 라떼"
+        case .vanillaLatte(temperature: .ice):
+            return "아이스 바닐라 라떼"
         case .coldbrew:
             return "콜드브루"
-        case .vanillaLatte(temp: .hot):
-            return "따뜻한 바닐라 라떼"
-        case .vanillaLatte(temp: .ice):
-            return "아이스 바닐라 라떼"
         }
     }
     
