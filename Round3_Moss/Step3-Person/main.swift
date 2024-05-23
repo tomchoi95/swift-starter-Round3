8f0923f6715e42a5b1d90995194ae964962d33f0
이재영 <ldc1000@naver.com>
2024-05-23 22:41:15 +0900

    PropertyObserver 사용해보기, 객체 의존성 완화해보기

--- Round3_Moss/Step3-Person/main.swift
+++ Round3_Moss/Step3-Person/main.swift
@@ -6,21 +6,31 @@
 //
 import Foundation
 
+
+
 class Person {
     let birthYears: Int
     var job: String
-    var money: Int
+    var methodsOfPayment: MethodsOfPayment
     var name: String
     
-    init(name: String, birthYears: Int, job: String, money: Int) {
-        self.name = name
+    init(birthYears: Int, job: String, methodsOfPayment: MethodsOfPayment, name: String) {
         self.birthYears = birthYears
         self.job = job
-        self.money = money
+        self.methodsOfPayment = methodsOfPayment
+        self.name = name
     }
     
     func orderCoffee(_ orders: [Coffee: Int], of coffeeShop: CoffeeShop) {
-        coffeeShop.takeOrder(orders, by: self)
+        coffeeShop.takeOrder(orders, by: name, payment: methodsOfPayment)
+    }
+}
+
+class MethodsOfPayment {
+    var money: Int
+    
+    init(money: Int) {
+        self.money = money
     }
     
     func enoughMoney(charge: Int) -> Bool {
@@ -33,26 +43,40 @@ class Person {
     
     func pay(charge: Int) -> Int? {
         guard self.enoughMoney(charge: charge) else { return nil }
-        
         self.money -= charge
         return charge
     }
 }
 
+
+
+
+
 class CoffeeShop {
     var sales: Int
     let menuBoard: [Coffee: Int]
-    var pickUpTable: [Coffee: Int]
+    var pickUpTable: [String: [Coffee: Int]] = [:] {
+        willSet {
+            let newCustomers = Array<String>(newValue.keys).filter { !Array<String>(pickUpTable.keys).contains($0) }
+            for customer in newCustomers {
+                if let orderStr = newValue[customer] {
+                    print(customer +
+                          " 님이 주문하신 " +
+                          orderStr.map { $0.key.convertKorean() + " " + String($0.value) + "잔" }.joined(separator: ", ") +
+                          "(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
+                }
+            }
+        }
+    }
     var barista: Person
     
-    init(sales: Int, menuBoard: [Coffee : Int], pickUpTable: [Coffee : Int], barista: Person) {
+    init(sales: Int, menuBoard: [Coffee : Int], barista: Person) {
         self.sales = sales
         self.menuBoard = menuBoard
-        self.pickUpTable = pickUpTable
         self.barista = barista
     }
     
-    func takeOrder(_ orders: [Coffee: Int],by customer: Person) {
+    func takeOrder(_ orders: [Coffee: Int], by customer: String, payment: MethodsOfPayment) {
         guard Set(menuBoard.keys).isSuperset(of: Set(orders.keys)) else {
             let haveNoMenus = Set(orders.keys)
                 .filter { !Set(menuBoard.keys).contains($0) }
@@ -65,30 +89,26 @@ class CoffeeShop {
         let totalPrice: Int = orders.map{ (menuBoard[$0.key] ?? 0) * $0.value }.reduce(0, +)
         print("총 \(totalPrice)원 결제 도와드리겠습니다.")
         
-        guard payed(charge: customer.pay(charge: totalPrice)) else { return }
+        guard payed(charge: totalPrice, payment: payment) else { return }
         print("결제되셨습니다.\n커피가 준비되면 성함 불러드리겠습니다.")
         
-        self.make(orders: orders, for: customer.name)
+        self.make(orders: orders, for: customer)
     }
     
     func make(orders: [Coffee:Int], for customer: String) {
-        let orderStr = orders.map { $0.key.convertKorean() + " " + String($0.value) + "잔" }
         
-        self.pickUpTable = orders.reduce(into: pickUpTable) { result, order in
-            result[order.key] = (result[order.key] ?? 0) + order.value
-        }
- 
-        print(customer +
-              " 님이 주문하신 " +
-              orderStr.joined(separator: ", ") +
-              "(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
+        
+        
+        pickUpTable[customer] = orders
+        //        }
+        
         return
     }
     
-    func payed(charge: Int?) -> Bool {
-        guard let charge = charge else {return false}
-        self.sales += charge
-
+    func payed(charge: Int?, payment: MethodsOfPayment) -> Bool {
+        guard let charge = charge else { return false }
+        guard let payed = payment.pay(charge: charge) else { return false }
+        self.sales += payed
         return true
     }
 }
@@ -124,15 +144,16 @@ enum Coffee: Hashable {
     }
 }
 
-let misterLee = Person(name: "misterLee" ,birthYears: 1990, job: "barista", money: 20_000)
-let missKim = Person(name: "missKim" ,birthYears: 1999, job: "student", money: 50_000)
+let misterLee = Person(birthYears: 1990, job: "barista", methodsOfPayment: MethodsOfPayment(money: 30000), name: "lee")
+let missKim = Person(birthYears: 1996, job: "student", methodsOfPayment: MethodsOfPayment(money: 50000), name: "kim")
 
 let yagombucks = CoffeeShop(sales: 0,
                             menuBoard: [.americano(temperature: .ice): 4500,
                                         .americano(temperature: .hot): 4800,
                                         .latte(temperature: .hot): 4800,
                                         .coldbrew: 5000],
-                            pickUpTable: [:],
                             barista: misterLee)
 
 missKim.orderCoffee([.americano(temperature: .hot): 3], of: yagombucks)
+
+
