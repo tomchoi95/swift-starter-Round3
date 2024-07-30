786e3f897c9cb761e5799dfc05befbea96b2febc
Crois0509 <crois0509@naver.com>
2024-07-30 15:56:25 +0900

    style: Peson의 메소드 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -4,7 +4,7 @@ var misterLee: Person = Person(name: "mrLee", age: 32, gender: .male, money: 250
 var missKim: Person = Person(name: "msKim", age: 25, gender: .female, money: 10000)
 var yagombucks: CoffeeShop = CoffeeShop(name: "yagombucks", barista: misterLee.name, sales: 0)
 
-missKim.buyCoffee(from: yagombucks, to: .Americano)
+missKim.order(.Americano, of: yagombucks, by: missKim.name)
 missKim.buyAllMenu(who: missKim, to: yagombucks)
 
 yagombucks.todaysCalculate(to: yagombucks)
