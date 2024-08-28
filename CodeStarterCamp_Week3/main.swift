0365e7eb7fab9294b02c3834540da764507e7043
Goohyun3436 <3436hyun@gmail.com>
2024-08-28 19:27:01 +0900

    add: Coffee 열거형 활용, 없는 메뉴 처리, guard-let으로 에러 처리

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -10,9 +10,14 @@ import Foundation
 
 var misterLee: Person = Person(name: "이야곰", money: 10000)
 var missKim: Person = Person(name: "김야곰", money: 20000)
-misterLee.buyCoffee(price: 2000, cups: 3)
-missKim.buyCoffee(price: 2000, cups: 53)
 
-let yagombucksMenu: [String: Int] = ["iceAmericano": 2000, "hotChoco": 4500]
+let yagombucksMenu: [Coffee: Int] = [.iceAmericano: 2000,.hotChoco: 4500]
 var yagombucks: CoffeeShop = CoffeeShop(menu: yagombucksMenu, barista: misterLee)
-yagombucks.order("iceAmericano", "iceChoco", "hotChoco")
+
+let orderList: [Coffee] = [.iceAmericano, .hotChoco]
+missKim.buyCoffee(items: orderList, shop: yagombucks)
+yagombucks.order(items: orderList)
+
+// let orderList: [Coffee] = [.iceAmericano, .hotChoco, .milkTea]
+// missKim.buyCoffee(items: orderList, shop: yagombucks)
+// yagombucks.order(items: orderList)
--- CodeStarterCamp_Week3/step1.swift
+++ CodeStarterCamp_Week3/step1.swift
@@ -1,48 +1,116 @@
+//
+//  step1.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 구현 on 8/28/24.
+//
+
+import Foundation
+
 struct Person {
     var name: String
     var money: Int
+
     init(name: String, money: Int) {
         self.name = name
         self.money = money
     }
-    mutating func buyCoffee(price: Int, cups: Int) {
-        let remainMoney: Int = self.money - (price * cups)
-        if remainMoney >= 0 {
-            self.money = remainMoney
-            print("\(name)이(가) 커피를 구매하였습니다. 현재 남은 돈은 \(self.money)원 입니다.")
-        } else {
+
+    mutating func buyCoffee(items: [Coffee], shop: CoffeeShop) {
+        guard let totalAmount: Int = calculateTotalAmount(items, at: shop) else {
+            print("\(name)이(가) 구매를 종료합니다.")
+            return
+        }
+        guard let remainMoney: Int = checkBuyAvailability(totalAmount) else {
             print("\(name)의 돈이 부족합니다. 현재 남은 돈은 \(self.money)원 입니다.")
+            return
+        }
+
+        self.money = remainMoney
+        printOrderList(items)
+    }
+
+    func calculateTotalAmount(_ items: [Coffee], at shop: CoffeeShop) -> Int? {
+        let menu: [Coffee: Int] = shop.menu
+        var totalAmount: Int = 0
+        for item in items {
+            guard let price: Int = menu[item] else {
+                print("\(item.rawValue)은(는) 없는 품목입니다.")
+                return nil
+            }
+            totalAmount += price
+        }
+        return totalAmount
+    }
+
+    func checkBuyAvailability(_ totalAmount: Int) -> Int? {
+        let remainMoney: Int = self.money - totalAmount
+        if remainMoney >= 0 {
+            return remainMoney
+        } else {
+            return nil
         }
     }
-}
 
-enum Coffee {
-    case iceAmericano
-    case hotAmericano
-    case iceChoco
-    case hotChoco
-    case milkTea
+    func printOrderList(_ items: [Coffee]) {
+        let orderListJoined: String = items.map{ $0.rawValue }.joined(separator: ", ")
+        print("\(name)이(가) \(orderListJoined)을(를) 구매하였습니다. 현재 남은 돈은 \(self.money)원 입니다.")
+    }
 }
 
 class CoffeeShop {
     var revenue: Int
-    var menu: [String: Int]
+    var menu: [Coffee: Int]
     var barista: Person
-    var pickUpTable: [String]
-    init(menu: [String: Int], barista: Person) {
+    var pickUpTable: [Coffee]
+
+    init(menu: [Coffee: Int], barista: Person) {
         self.revenue = 0
         self.menu = menu
         self.barista = barista
         self.pickUpTable = []
     }
-    func order(_ items: String...) {
-        for item in items {
-            if self.menu[item] != nil {
-                self.pickUpTable.append(item)
-            } else {
-                print("\(item)은(는) 없는 품목입니다.")
-            }
+
+    func order(items: [Coffee]) {
+        guard let (pickUpTable, revenue): ([Coffee], Int) = checkItemAvailability(items) else {
+            print("주문을 종료합니다.")
+            return
         }
-        print("\(self.barista.name)(이)가 \(self.pickUpTable) 주문을 접수하였습니다.")
+
+        self.pickUpTable = pickUpTable
+        self.revenue = revenue
+        printPickUpTable()
+        printRevenue()
     }
+
+    func checkItemAvailability(_ items: [Coffee]) -> ([Coffee], Int)? {
+        var pickUpTable: [Coffee] = []
+        var revenue: Int = 0
+        for item in items {
+            guard let price: Int = self.menu[item] else {
+                print("\(item)은(는) 없는 품목입니다.")
+                return nil
+            }
+            pickUpTable.append(item)
+            revenue += price
+        }
+        return (pickUpTable, revenue)
+    }
+
+    func printPickUpTable() {
+        let pickUpTableJoined: String = self.pickUpTable.map{ $0.rawValue }.joined(separator: ", ")
+        print("바리스타 \(self.barista.name)(이)가 \(pickUpTableJoined) 주문을 접수하였습니다.")
+    }
+
+    func printRevenue() {
+        print("오늘의 총매출은 \(self.revenue)원 입니다.")
+    }
+}
+
+enum Coffee: String {
+    case iceAmericano
+    case hotAmericano
+    case iceChoco
+    case hotChoco
+    case milkTea
 }
