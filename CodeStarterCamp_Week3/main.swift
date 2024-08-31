89a9f3183c71cfaea2592e9f92e29b0d2de8d6ae
Elin <byrr113@gmail.com>
2024-08-31 17:53:44 +0900

    main.swift 파일 추가

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+yagombucks.getOrder(customer: missKim, order: "카페모카")
+yagombucks.makeCoffee(customerNumber: 0)
 
+print("""
+
+pickUpTable: \(yagombucks.pickUpTable)
+매출액: \(yagombucks.sales)
+손님 잔액: \(missKim.money)
+
+""")
+
+yagombucks.giveCoffee(customer: missKim, customerNumber: 0)
+print("""
+
+pickUpTale: \(yagombucks.pickUpTable)
+customer.onHand: \(missKim.onHand)
+""")
