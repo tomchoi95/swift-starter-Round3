ba8264d4005a61f5e9239a49c322557febe4a036
Goohyun3436 <3436hyun@gmail.com>
2024-09-03 22:47:30 +0900

    add: Coffee 열거형에 커피 가격 연산 프로퍼티 활용

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,13 +8,19 @@
 
 import Foundation
 
+// 경우 01: 정상 주문
+let yagombucksMenu: [Coffee] = [.iceAmericano, .hotChoco, .milkTea]
+let orderList: [Coffee] = [.iceAmericano, .hotChoco]
+
+// 경우 02: 없는 메뉴 주문
+//let yagombucksMenu: [Coffee] = [.iceAmericano]
+//let orderList: [Coffee] = [.iceAmericano, .hotAmericano]
+
+// 경우 03: 잔액 부족
+//let yagombucksMenu: [Coffee] = [.iceAmericano, .hotAmericano, .iceChoco, .hotChoco, .milkTea]
+//let orderList: [Coffee] = [.iceAmericano, .hotAmericano, .iceAmericano, .iceChoco, .hotChoco, .hotChoco, .milkTea]
+
 var misterLee: Person = Person(name: "이야곰")
 var missKim: Person = Person(name: "김야곰", money: 20000)
-
-let yagombucksMenu: [Coffee] = [.iceAmericano, .hotChoco, .milkTea]
-//let yagombucksMenu: [Coffee] = [.iceAmericano, .hotAmericano, .iceChoco, .hotChoco, .milkTea]
 var yagombucks: CoffeeShop = CoffeeShop(menu: yagombucksMenu, barista: misterLee)
-
-let orderList: [Coffee] = [.iceAmericano, .hotChoco]
-//let orderList: [Coffee] = [.iceAmericano, .hotAmericano, .iceAmericano, .iceChoco, .hotChoco, .hotChoco, .milkTea]
 missKim.buyCoffee(orderList, shop: yagombucks)
