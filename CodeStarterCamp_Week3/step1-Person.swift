0aef66130ba50bfb81e2dd21fc802100c896ad48
Elin <byrr113@gmail.com>
2024-09-03 22:07:16 +0900

    fix: class Person 초기화 및 메소드 이름 변경

--- CodeStarterCamp_Week3/step1-Person.swift
+++ CodeStarterCamp_Week3/step1-Person.swift
@@ -11,22 +11,28 @@ class Person {
     var name: String
     var money = 0
     var enoughMoney: Bool = true
-    var number: Int?
+    var waitingNumber: Int?
     var onHand: [String] = []
     
     init(name: String, money: Int) {
         self.name = name
         self.money = money
     }
+    
+    init(name: String, money: Int, onHand: [String]) {
+        self.name = name
+        self.money = money
+        self.onHand = onHand
+    }
 
-    func buyCoffee(price: Int) {
+    func buy(price: Int) {
         guard money >= price else {
             return self.enoughMoney = false
         }
         self.money -= price
     }
     
-    func getCoffee(receive: String) {
-        self.onHand.append(receive)
+    func get(coffee: String) {
+        self.onHand.append(coffee)
     }
 }
