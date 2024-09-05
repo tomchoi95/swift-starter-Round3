958ffec195a4353414495fea7512eaabcf226456
H.GOO <rngus3436@g.kmou.ac.kr>
2024-09-05 11:35:45 +0900

    refactor: missKim의 buyCoffee 메서드 매개변수에 Array 바로 넘겨주도록 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,19 +8,7 @@
 
 import Foundation
 
-// 경우 01: 정상 주문
-let yagombucksMenu: [Coffee] = [.iceAmericano, .hotChoco, .milkTea]
-let orderList: [Coffee] = [.iceAmericano, .hotChoco]
-
-// 경우 02: 없는 메뉴 주문
-//let yagombucksMenu: [Coffee] = [.iceAmericano]
-//let orderList: [Coffee] = [.iceAmericano, .hotAmericano]
-
-// 경우 03: 잔액 부족
-//let yagombucksMenu: [Coffee] = [.iceAmericano, .hotAmericano, .iceChoco, .hotChoco, .milkTea]
-//let orderList: [Coffee] = [.iceAmericano, .hotAmericano, .iceAmericano, .iceChoco, .hotChoco, .hotChoco, .milkTea]
-
 var misterLee: Person = Person(name: "이야곰")
 var missKim: Person = Person(name: "김야곰", money: 20000)
-var yagombucks: CoffeeShop = CoffeeShop(menu: yagombucksMenu, barista: misterLee)
-missKim.buyCoffee(orderList, shop: yagombucks)
+var yagombucks: CoffeeShop = CoffeeShop(menu: [.iceAmericano, .hotChoco, .milkTea], barista: misterLee)
+missKim.buyCoffee([.iceAmericano, .hotChoco], shop: yagombucks)
