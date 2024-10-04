ba5b8fbdbbf2db17dd4af2e5e58e121453bbb0ed
tom choi <qjatn0545@naver.com>
2024-10-04 12:07:58 +0900

    person 타입 정의

---
+++ CodeStarterCamp_Week3/cafe.swift
@@ -0,0 +1,14 @@
+//
+//  cafe.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 최범수 on 2024-10-03.
+//
+
+struct Person {
+    var money : Int = 0
+    
+    func buyCoffee() {
+        print("커피 구입")
+    }
+}
